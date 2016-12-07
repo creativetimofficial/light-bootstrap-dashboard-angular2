985c6eafeca6bf3d351dbd09ceaf5ba54193c28e
Ciprian Chelaru <chelaru_ciprian@yahoo.com>
2016-12-07 20:05:05 +0200

    sters body-parser

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
 var app = express();
 
 var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
-
-
-
-// CONTACTS API ROUTES BELOW
