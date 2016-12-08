a1181a417f6619047aea7ece518a8429bdf00d5f
Ciprian Chelaru <chelaru_ciprian@yahoo.com>
2016-12-08 18:29:52 +0200

    Add reflect-metadata

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import {enableProdMode} from '@angular/core';
+import 'reflect-metadata';
+
 enableProdMode();
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
