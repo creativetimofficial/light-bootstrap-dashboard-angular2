947cea54f3c1bd6739ae6012fc3931478e4de268
Ciprian Chelaru <chelaru_ciprian@yahoo.com>
2016-12-12 11:13:01 +0200

    Stars reselect-metadata si profile

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import {enableProdMode} from '@angular/core';
-import 'reflect-metadata';
 
 enableProdMode();
 const platform = platformBrowserDynamic();
