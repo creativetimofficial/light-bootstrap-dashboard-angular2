1e93c1e8ddc95c03c33f6f515cdf031a237f828f
Ciprian Chelaru <chelaru_ciprian@yahoo.com>
2016-12-14 15:46:21 +0200

    Resolve conflict

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var app = express();
-
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
