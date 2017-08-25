5fd937caa75eb132015e4d8152fbf4d268c648f8
chelaruc <chelaru_ciprian@yahoo.com>
2017-08-25 12:16:16 +0300

    Deleted node_modules

--- node_modules/@angular/cli/utilities/stats.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function statsToString(json: any, statsConfig: any): string;
-export declare function statsWarningsToString(json: any, statsConfig: any): string;
-export declare function statsErrorsToString(json: any, statsConfig: any): string;
--- node_modules/@angular/router/testing.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @license Angular v4.3.6
- * (c) 2010-2017 Google, Inc. https://angular.io/
- * License: MIT
- */ 
- export * from './testing/index'
