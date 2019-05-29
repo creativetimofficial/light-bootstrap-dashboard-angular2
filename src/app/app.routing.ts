0e9c88f30f620fd4a6ce3338269642cec58387e4
chelaruc <chelaruciprian10@gmail.com>
2019-05-29 18:13:29 +0300

    added hash

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -28,7 +28,9 @@ const routes: Routes =[
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes,{
+       useHash: true
+    })
   ],
   exports: [
   ],
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -52,7 +52,10 @@ export class NavbarComponent implements OnInit{
 
     getTitle(){
       var titlee = this.location.prepareExternalUrl(this.location.path());
-      titlee = titlee.split('/').pop();
+      if(titlee.charAt(0) === '#'){
+          titlee = titlee.slice( 1 );
+      }
+
       for(var item = 0; item < this.listTitles.length; item++){
           if(this.listTitles[item].path === titlee){
               return this.listTitles[item].title;
