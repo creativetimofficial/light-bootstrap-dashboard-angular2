bf97d95cdf0c1b3069ae9dd219a46969130fe070
taqishah1214 <60063724+taqishah1214@users.noreply.github.com>
2022-04-30 06:44:43 +0500

    Angular 13 update, routing issue fixed, angular.json updated, build issue fixed and .gitignore updated

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -16,7 +16,7 @@ const routes: Routes =[
     children: [
         {
       path: '',
-      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+      loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(x => x.AdminLayoutModule)
   }]},
   {
     path: '**',
