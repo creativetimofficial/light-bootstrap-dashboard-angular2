8ab4d32be41bf70cf332f3b941de5f4f2687c359
Camilo Mosquera <61419653+cmosquera1998@users.noreply.github.com>
2023-02-22 23:41:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('dist/encuentros-matrimoniales'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join('/dist/encuentros-matrimoniales/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
