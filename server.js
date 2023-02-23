b05ff37c280eec8801f277c1d3676d5ffaa39cd5
Camilo Mosquera <61419653+cmosquera1998@users.noreply.github.com>
2023-02-23 00:32:40 -0500

    Update and rename server.txt to server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+ 'dist/encuentros-matrimoniales'));
+app.use(express.static(__dirname+ '/dist/'));
 
 app.get('/*', function(req,res) {
 console.log("Fetching from __dirname.." + __dirname);     
-const fpath = path.join(__dirname+ '/dist/encuentros-matrimoniales/index.html');
+const fpath = path.join(__dirname+ '/dist/index.html');
 console.log("Fetching from.." + fpath);
 res.sendFile(fpath);
 });
