8a4354969d863675505f5810b834edf838e26235
Andrés Esteban Arévalo Rojas <aarevalor@unbosque.edu.co>
2023-03-16 21:51:10 -0500

    -- Login
    Registro y login vistas agregadas, primer micro servicio agregado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from "../user/user.service";
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
@@ -12,18 +14,32 @@ export class LoginComponent implements OnInit {
   email: string;
   password: string;
 
-  constructor(public userService: UsersService, public router: Router) {}
+  constructor(public userService: UsersService, public router: Router, public http: HttpClient) {}
 
   login() {
+    debugger
+    const url = 'https://encuentro-matrimonial-backend.herokuapp.com/login';
+    const body = {
+      username: 'admin',
+      password: 'admin'
+    };
+    return this.http.post(url, body);
+  }
+
+  // login() {
+  //   const user = { email: this.email, password: this.password };
+  //   this.userService.login(user).subscribe(
+  //     data => {
+  //       //this.userService.setToken(data.token);
+  //       this.router.navigateByUrl('/');
+  //     },
+  //     error => {
+  //       console.log(error);
+  //     });
+  // }
+  register() {
     const user = { email: this.email, password: this.password };
-    this.userService.login(user).subscribe(
-      data => {
-        //this.userService.setToken(data.token);
-        this.router.navigateByUrl('/');
-      },
-      error => {
-        console.log(error);
-      });
+    this.router.navigateByUrl('/register');
   }
   ngOnInit() {
   }
