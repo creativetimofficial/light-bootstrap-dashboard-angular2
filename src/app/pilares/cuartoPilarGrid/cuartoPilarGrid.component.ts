511c59bf8f6754fd83a895ce9a0c30d8710b6c29
BeymarBurgos <82980012+BeymarBurgos@users.noreply.github.com>
2023-05-11 19:14:02 -0500

    Popups delete

--- src/app/pilares/cuartoPilarGrid/cuartoPilarGrid.component.ts
+++ src/app/pilares/cuartoPilarGrid/cuartoPilarGrid.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import * as XLSX from 'xlsx';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDownloadDialogComponent } from 'app/shared/confirm-download-dialog/confirm-download-dialog.component'; 
+import { ConfirmDeleteComponent } from 'app/shared/confirm-delete/confirm-delete.component';
 
 declare interface TableData {
     headerRow: string[];
@@ -106,6 +107,16 @@ export class CuartoPilarGridComponent implements OnInit {
       console.log(elementId);
       this.router.navigate(['/editarCuartoPilar', elementId]);
       
+    }
+    openDialogDelete(row):void{
+      const dialogRef = this.dialog.open(ConfirmDeleteComponent,{
+      });
+      dialogRef.afterClosed().subscribe(res => {
+        console.log(res);
+        if(res){
+          this.deleteRow(row);
+        }
+      })
     }
     public deleteRow(row) {
       const params = { id: row.id };
